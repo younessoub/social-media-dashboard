c091b57ea45e629bb408b38ddfab46fb138aa094
oubelkacemy@gmail.com <oubelkacemy@gmail.com>
2023-07-18 02:29:25 +0100

    Dark mode done

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const darkWhite = document.querySelectorAll('.dark-white');
+const darkDesaturatedBlue = document.querySelectorAll('.dark-DarkDesaturatedBlue');
+const darkDarkDesaturatedBlue = document.querySelectorAll('.dark-DarkDesaturatedBlue');
+const darkVeryDarkBlueTop = document.querySelectorAll('.dark-VeryDarkBlueTop');
+const darkVeryDarkBlue = document.querySelectorAll('.dark-VeryDarkBlue');
+
+const darkModeToggle = document.querySelector('.slider')
+
+
+darkModeToggle.addEventListener('click', ()=>{
+  toggleDarkMode()
+})
+
+
+function toggleDarkMode() {
+  darkWhite.forEach(e => {
+    e.classList.toggle('color-white')
+  })
+  darkDesaturatedBlue.forEach(e => {
+    e.classList.toggle('color-DesaturatedBlue')
+  })
+  darkDarkDesaturatedBlue.forEach(e => {
+    e.classList.toggle('background-DarkDesaturatedBlue')
+  })
+  darkVeryDarkBlueTop.forEach(e => {
+    e.classList.toggle('background-VeryDarkBlueTop')
+  })
+  darkVeryDarkBlue.forEach(e => {
+    e.classList.toggle('background-VeryDarkBlue')
+  })
+
+}
